45af865ddaa1a3bd6183dc40969965c778390d20
= <=>
2019-09-05 13:57:11 +0800

    helloworld commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello China");
+        System.out.println("Hello Zhejiang");
+        System.out.println("Hello Hangzhou");
+    }
+}
